81a2e65d60fa85716b49551709494a4485637056
david.krastev <david.krastev@paysafe.com>
2026-05-22 11:44:00 +0300

    [DC-1000] add specific handling for not_found exception

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
 import { membersController } from "./controllers/members.controller";
 
-const app = new Elysia().use(membersController).listen(3000);
+const app = new Elysia()
+    .onError(({ code, error, set }) => {
+        if (code === "NOT_FOUND") {
+            set.status = 404;
+            return { message: error.message };
+        }
+    })
+    .use(membersController)
+    .listen(3000);
 
 console.log(`Server running at ${app.server?.hostname}:${app.server?.port}`);
